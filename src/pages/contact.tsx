cadcb98bc07a7c675ca72d7b2352567317e59011
Neil Huyton <neil.huyton@gmail.com>
2019-10-02 20:29:28 +0100

    WIP

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -7,47 +7,55 @@ const ContactPage = () => {
   return (
     <Layout>
       <SEO title="Contact" />
-      <section>
-        <h3>Form</h3>
-        <form method="post" netlify-honeypot="bot-field" data-netlify="true">
-          <input type="hidden" name="bot-field" />
-          <div className="row gtr-uniform">
-            <div className="col-6 col-12-xsmall">
-              <input
-                type="text"
-                name="demo-name"
-                id="demo-name"
-                value=""
-                placeholder="Name"
-              />
-            </div>
-            <div className="col-6 col-12-xsmall">
-              <input
-                type="email"
-                name="demo-email"
-                id="demo-email"
-                value=""
-                placeholder="Email"
-              />
-            </div>
-            <div className="col-12">
-              <textarea
-                name="demo-message"
-                id="demo-message"
-                placeholder="Enter your message"
-                rows={6}
-              ></textarea>
-            </div>
-            <div className="col-12">
-              <ul className="actions">
-                <li>
-                  <input type="submit" value="Send Message" />
-                </li>
-              </ul>
-            </div>
+      <article className="post">
+        <header>
+          <div className="title">
+            <h2>Contact</h2>
+            <p>sidijn</p>
           </div>
-        </form>
-      </section>
+        </header>
+        <section>
+          <h3>Form</h3>
+          <form method="post" netlify-honeypot="bot-field" data-netlify="true">
+            <input type="hidden" name="bot-field" />
+            <div className="row gtr-uniform">
+              <div className="col-6 col-12-xsmall">
+                <input
+                  type="text"
+                  name="demo-name"
+                  id="demo-name"
+                  value=""
+                  placeholder="Name"
+                />
+              </div>
+              <div className="col-6 col-12-xsmall">
+                <input
+                  type="email"
+                  name="demo-email"
+                  id="demo-email"
+                  value=""
+                  placeholder="Email"
+                />
+              </div>
+              <div className="col-12">
+                <textarea
+                  name="demo-message"
+                  id="demo-message"
+                  placeholder="Enter your message"
+                  rows={6}
+                ></textarea>
+              </div>
+              <div className="col-12">
+                <ul className="actions">
+                  <li>
+                    <input type="submit" value="Send Message" />
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </form>
+        </section>
+      </article>
     </Layout>
   )
 }
